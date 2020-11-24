11a0aae79c7b045d1b4e5eff70763cb4c39e3709
root <crazyescape88@gmail.com>
2020-11-24 22:33:00 +0900

    Line drawing in surfaceview

--- app/src/main/java/com/example/clicker/TitleActivity.kt
+++ app/src/main/java/com/example/clicker/TitleActivity.kt
@@ -1,14 +1,17 @@
 package com.example.clicker
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle;
 
 class TitleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation=ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContentView(R.layout.activity_title);
 
         val view : TitleSurfaceView = findViewById(R.id.backgroundView);
+
 
     }
 }
--- app/src/main/java/com/example/clicker/TitleSurfaceView.kt
+++ app/src/main/java/com/example/clicker/TitleSurfaceView.kt
@@ -1,14 +1,92 @@
 package com.example.clicker
 
+import android.content.ContentValues.TAG
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
+import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView;
+import java.util.*
 
-class MainThread : Thread(){
+class MainThread(context: Context , holder: SurfaceHolder , view:SurfaceView) : Thread(){
     var number:Int = 0
+    private var context: Context = context
+    private var holder:SurfaceHolder = holder
+    private val view : SurfaceView = view
+
+    private var initLine:Path = Path()
+    private var drawLines : Queue<Path> = LinkedList()
+
+
+
+    fun CalculateBackgroundPath(){
+        val viewHeight : Float = view.height.toFloat()
+        val viewWidth : Float = view.width.toFloat()
+        val dx_top : Float = 0.071428F*viewWidth
+        val dx_bottom : Float =0.071428F*viewWidth*5
+
+        initLine.reset()
+        initLine.moveTo(0F,viewHeight*0.65F)
+        initLine.lineTo(viewWidth, viewHeight*0.65F)
+
+        for(x in 1..13) {
+            initLine.moveTo(dx_top*x, viewHeight * 0.65F)
+            initLine.lineTo(-2*viewWidth+(dx_bottom*x), viewHeight )
+        }
+
+        /*
+        initLine.moveTo(viewWidth*0.5F , viewHeight*0.65F)
+        initLine.lineTo(viewWidth*0.5F,viewHeight)
+
+         */
+
+
+
+    }
+
+
     override fun run() {
-        super.run()
+        //super.run();
+        var canvas: Canvas? = null
+        val paint : Paint = Paint()
+
+        CalculateBackgroundPath()
+
+        while(true){
+            canvas = this.holder.lockCanvas()
+            try {
+
+                val paint : Paint = Paint()
+                paint.setColor(Color.RED)
+                paint.setStyle(Paint.Style.STROKE);
+                if (canvas != null) {
+                    canvas.drawPath(initLine ,paint)
+                    /*
+                    for( s in initLine )
+                        canvas.drawPath(s ,paint)
+
+                     */
+
+                }
+            } catch ( e :Exception){
+                Log.e(TAG, "[Drawing Thread]", e);
+            }
+            finally {
+                if(canvas != null)
+                    this.holder.unlockCanvasAndPost(canvas)
+            }
+
+            sleep(20)
+
+
+
+        }
+
+
     }
 
 
@@ -20,11 +98,13 @@ class TitleSurfaceView : SurfaceView , SurfaceHolder.Callback {
     var width_:Int = 0
     private val context_:Context
     private val holder_ :SurfaceHolder
+    private val thread : MainThread;
 
     init {
         this.context_= context;
         this.holder_ = getHolder();
         holder_.addCallback(this)
+        thread = MainThread(context , holder_ , this)
     }
 
     constructor(context: Context):super(context){
@@ -44,8 +124,14 @@ class TitleSurfaceView : SurfaceView , SurfaceHolder.Callback {
         getHolder().setFixedSize(x,y)
     }
 
+    public fun startThread(){
+        thread.start();
+    }
+
     override fun surfaceCreated(holder: SurfaceHolder) {
         //TODO("Not yet implemented")
+
+        startThread()
     }
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
         //TODO("Not yet implemented")
